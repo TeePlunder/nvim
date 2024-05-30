f0fc28c1e801bccc2156a9d2ac0c910383396e27
TeePlunder <leon.bergamnn23@gmail.com>
2024-05-30 08:13:44 +0200

    fix(fileformat): set to dos

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,1 +1,12 @@
 vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
+
+-- Function to set file format to CRLF
+local function set_fileformat_to_crlf()
+  vim.bo.fileformat = "dos"
+end
+
+-- Auto command to set file format to CRLF on file save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = set_fileformat_to_crlf,
+})
