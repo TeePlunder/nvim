fb932c11c072684a71e1e1b5d48d5caecfdda6de
TeePlunder <leon.bergamnn23@gmail.com>
2024-05-31 09:37:56 +0200

    adjust example.lua

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -104,7 +104,7 @@ return {
       init = function()
         require("lazyvim.util").lsp.on_attach(function(_, buffer)
           -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
         end)
       end,
@@ -147,7 +147,6 @@ return {
         "lua",
         "markdown",
         "markdown_inline",
-        "python",
         "query",
         "regex",
         "tsx",
@@ -178,17 +177,6 @@ return {
     event = "VeryLazy",
     opts = function(_, opts)
       table.insert(opts.sections.lualine_x, "😄")
-    end,
-  },
-
-  -- or you can return new options to override all the defaults
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return {
-        --[[add your custom lualine config here]]
-      }
     end,
   },
 
