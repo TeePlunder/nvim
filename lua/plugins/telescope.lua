e5d7afee1be01c7e7629e49ce5f9be61b50bd35e
TeePlunder <leon.bergmann23@gmail.com>
2024-08-27 20:07:13 +0200

    telescope: add default keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,20 @@
 return {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter' }
+    -- Telescope and its dependencies
+    {
+        'nvim-telescope/telescope.nvim', 
+        branch = '0.1.x',
+        dependencies = { 
+            'nvim-lua/plenary.nvim', 
+            'nvim-treesitter/nvim-treesitter', 
+        },
+	config = function()
+	    -- Default key mappings for Telescope
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        end,
+
     }
+}
