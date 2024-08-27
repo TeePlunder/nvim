ca1ebed371249777c48d512ea265a45db6db0893
TeePlunder <leon.bergmann23@gmail.com>
2024-08-27 18:43:50 +0200

    gruvbox

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,7 @@
+return {
+{ "morhetz/gruvbox", priority = 1000 , config = function()
+	vim.g.gruvbox_contrast_dark = 'soft'  -- Options: 'soft', 'medium', 'hard'
+            vim.g.gruvbox_invert_selection = '0'
+            vim.cmd("colorscheme gruvbox")  -- Set Gruvbox as the default color scheme
+    end}
+}
