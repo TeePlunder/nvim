a4f02405a5e67123040a91ee2fd390ccf8c927ab
TeePlunder <leon.bergmann23@gmail.com>
2024-08-29 19:05:14 +0200

    tsserver add to lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,8 @@ return {
 		config = function()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({})
+
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 			vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
