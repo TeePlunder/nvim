ff1c9e7f18c3fb5d9283cc1224aa84e7d77154ca
TeePlunder <leon.bergmann23@gmail.com>
2024-08-29 19:33:47 +0200

    treesitter: enable auto install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,22 @@
 return {
-  -- Other plugin configurations
+	-- Other plugin configurations
 
-  -- Tree-sitter configuration
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require'nvim-treesitter.configs'.setup {
-        ensure_installed = {"lua", "markdown", "markdown_inline", "javascript"}, -- Install all parsers (or specify a list like {"lua", "python", "javascript"})
-        highlight = {
-          enable = true,              -- false will disable the whole extension
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true
-        },
-        -- Add more configuration options as needed
-      }
-    end
-  }
+	-- Tree-sitter configuration
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				autoinstall = true,
+				highlight = {
+					enable = true, -- false will disable the whole extension
+					additional_vim_regex_highlighting = false,
+				},
+				indent = {
+					enable = true,
+				},
+				-- Add more configuration options as needed
+			})
+		end,
+	},
 }
-
