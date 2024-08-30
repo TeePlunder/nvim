a9aeac171c0a03ff79382406ed8517d88723ab05
TeePlunder <leon.bergmann23@gmail.com>
2024-08-30 21:08:30 +0200

    fix: renamer keys

--- lua/plugins/renamer.lua
+++ lua/plugins/renamer.lua
@@ -1,20 +1,10 @@
 return {
-	"filipdutescu/renamer.nvim",
-	config = function()
-		require("renamer").setup()
-	end,
-	keys = {
-		{
-			"i",
-			"<F2>",
-			'<cmd>lua require("renamer").rename()<cr>',
-			{ noremap = true, silent = true, desc = "Rename in Insert Mode" },
-		},
-		{
-			{ "n", "v" },
-			"<leader>cr",
-			'<cmd>lua require("renamer").rename()<cr>',
-			{ noremap = true, silent = true, desc = "Rename in Normal/Visual Mode" },
-		},
-	},
+  "filipdutescu/renamer.nvim",
+  config = function()
+    require("renamer").setup()
+  end,
+  keys = {
+    { "i", "<F2>",       '<cmd>lua require("renamer").rename()<cr>', { desc = "Rename in Insert Mode" } },
+    { "n", "<leader>cr", '<cmd>lua require("renamer").rename()<cr>', { desc = "Rename" } },
+  },
 }
