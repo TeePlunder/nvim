7e76fb53152c5b56291af2d4d353c8092ba7f4e2
TeePlunder <leon.bergmann23@gmail.com>
2024-08-30 16:11:48 +0200

    none-ls: change keymap to <leader>f to format file

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,8 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 
@@ -11,13 +11,13 @@ return {
 				null_ls.builtins.formatting.stylua,
 				-- TODO: try it for code actions
 				-- null_ls.builtins.code_actions.eslint_d,
-require("none-ls.diagnostics.eslint_d"),
-        -- TODO: try if something is not working
+				require("none-ls.diagnostics.eslint_d"),
+				-- TODO: try if something is not working
 				-- null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.prettierd
+				null_ls.builtins.formatting.prettierd,
 			},
 		})
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
 	end,
 }
