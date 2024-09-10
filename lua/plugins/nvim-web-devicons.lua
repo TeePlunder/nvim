7d8727d646069f4c44e123522fd82bfad9b3cfe5
TeePlunder <leon.bergmann23@gmail.com>
2024-09-10 12:13:03 +0200

    format all files and remove unused code

--- lua/plugins/nvim-web-devicons.lua
+++ lua/plugins/nvim-web-devicons.lua
@@ -1,17 +1,12 @@
 return {
-    -- Other plugin configurations
-    {
-        'nvim-tree/nvim-web-devicons',
-        config = function()
-            require('nvim-web-devicons').setup {
-                -- globally enable different icon highlights (optional)
-                -- default is true
-                color_icons = true;
-                -- globally enable default icons (default to false)
-                default = true;
-            }
-        end
-    },
-    -- Your other plugins here
+  'nvim-tree/nvim-web-devicons',
+  config = function()
+    require('nvim-web-devicons').setup {
+      -- globally enable different icon highlights (optional)
+      -- default is true
+      color_icons = true,
+      -- globally enable default icons (default to false)
+      default = true,
+    }
+  end
 }
-
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,16 @@
 return {
-	-- Other plugin configurations
-
-	-- Tree-sitter configuration
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				autoinstall = true,
-				highlight = {
-					enable = true, -- false will disable the whole extension
-					additional_vim_regex_highlighting = false,
-				},
-				indent = {
-					enable = true,
-				},
-				-- Add more configuration options as needed
-			})
-		end,
-	},
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      autoinstall = true,
+      highlight = {
+        enable = true, -- false will disable the whole extension
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+    })
+  end,
 }
