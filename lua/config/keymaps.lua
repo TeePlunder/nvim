3578c59134629a4b5617283ee03a859121fc0bea
TeePlunder <leon.bergamnn23@gmail.com>
2024-09-16 15:29:04 +0200

    keymaps: add to last buffer keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,3 +16,5 @@ vim.keymap.set("n", "<leader>gr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left>
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>svc", "<cmd>source ~/.config/nvim/init.lua<CR>")
+
+vim.keymap.set("n", "<leader>bb", "<cmd>b#<CR>", {desc = "To last Buffer"})
