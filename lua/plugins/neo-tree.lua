2f098efdb5c0af270dd63b489f225ee713af40db
TeePlunder <leon.bergamnn23@gmail.com>
2024-09-17 13:10:43 +0200

    feat(neotree): follow current file

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,6 +10,13 @@ return {
     window = {
       position = "right", -- This should correctly set the Neo-tree to open on the right side
     },
+    filesystem = {
+      follow_current_file = {
+        enabled = true,     -- This will find and focus the file in the active buffer every time
+        --              -- the current file is changed while the tree is open.
+        leave_dirs_open = false,     -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      }
+    },
     event_handlers = {
       {
         event = "file_opened",
