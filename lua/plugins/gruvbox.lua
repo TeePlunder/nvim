c032bf045a87010a37b884b2de160cba84c902db
TeePlunder <leon.bergamnn23@gmail.com>
2024-09-18 07:16:21 +0200

    fix(gruvbox): use other gruvbox theme that supports transparent mode

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,11 +1,8 @@
 return {
-  {
-    "morhetz/gruvbox",
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_contrast_dark = 'soft' -- Options: 'soft', 'medium', 'hard'
-      vim.g.gruvbox_invert_selection = '0'
-      vim.cmd("colorscheme gruvbox")       -- Set Gruvbox as the default color scheme
-    end
-  }
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = true,
+  opts = {
+    transparent_mode = true,
+  },
 }
