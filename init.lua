73d9fdb0bb49b4ca6b3176bd403f9fa22219cf16
TeePlunder <leon.bergamnn23@gmail.com>
2024-09-18 07:16:21 +0200

    fix(gruvbox): use other gruvbox theme that supports transparent mode

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@ require("config.lazy")
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
