6589b4bb1af5a10d75a160b9639d180c90265671
Leon Bergmann <leonbergmann@MacBook-Air-von-Admin-2.local>
2025-01-03 14:15:25 +0100

    none ls prefer file when formatting with prettier

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,25 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				-- TODO: try it for code actions
-				-- null_ls.builtins.code_actions.eslint_d,
-				require("none-ls.diagnostics.eslint_d"),
-				-- TODO: try if something is not working
-				-- null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.prettierd,
-			},
-		})
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        -- TODO: try it for code actions
+        -- null_ls.builtins.code_actions.eslint_d,
+        require("none-ls.diagnostics.eslint_d"),
+        -- TODO: try if something is not working
+        null_ls.builtins.formatting.prettier.with({
+          extra_args = { "--config-precedence", "prefer-file" },
+        }),
+        -- null_ls.builtins.formatting.prettierd,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, { desc = "Format Code" })
-	end,
+    vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, { desc = "Format Code" })
+  end,
 }
