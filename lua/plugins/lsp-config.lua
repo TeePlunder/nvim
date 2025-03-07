8dc2e57dd66d29eaf96abc7a868e71c494fb4757
Leon Bergmann <l.bergmann@sweetconnect.io>
2025-03-07 15:50:07 +0100

    feat(lsp-config): add a border on my hover

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,77 +1,83 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			automatic_installation = true,
-			ensure_installed = { "lua_ls", "gopls" },
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      automatic_installation = true,
+      ensure_installed = { "lua_ls", "gopls" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
+      -- Add border to hover
+      vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+        vim.lsp.handlers.hover, {
+          border = "rounded" -- You can use "single", "double", "rounded", etc.
+        }
+      )
 
-			-- keymaps
+      -- keymaps
 
-			local function filter(arr, fn)
-				if type(arr) ~= "table" then
-					return arr
-				end
+      local function filter(arr, fn)
+        if type(arr) ~= "table" then
+          return arr
+        end
 
-				local filtered = {}
-				for k, v in pairs(arr) do
-					if fn(v, k, arr) then
-						table.insert(filtered, v)
-					end
-				end
+        local filtered = {}
+        for k, v in pairs(arr) do
+          if fn(v, k, arr) then
+            table.insert(filtered, v)
+          end
+        end
 
-				return filtered
-			end
+        return filtered
+      end
 
-			local function filterReactDTS(value)
-				return string.match(value.filename, "react/index.d.ts") == nil
-			end
+      local function filterReactDTS(value)
+        return string.match(value.filename, "react/index.d.ts") == nil
+      end
 
-			local function on_list(options)
-				local items = options.items
-				if #items > 1 then
-					items = filter(items, filterReactDTS)
-				end
+      local function on_list(options)
+        local items = options.items
+        if #items > 1 then
+          items = filter(items, filterReactDTS)
+        end
 
-				vim.fn.setqflist({}, " ", { title = options.title, items = items, context = options.context })
-				vim.api.nvim_command("cfirst") -- or maybe you want 'copen' instead of 'cfirst'
-			end
+        vim.fn.setqflist({}, " ", { title = options.title, items = items, context = options.context })
+        vim.api.nvim_command("cfirst") -- or maybe you want 'copen' instead of 'cfirst'
+      end
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
 
-			vim.keymap.set("n", "gd", function()
-				vim.lsp.buf.definition({ on_list = on_list })
-			end, { desc = "Go to Definition" })
-			-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to References" })
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
-		end,
-	},
+      vim.keymap.set("n", "gd", function()
+        vim.lsp.buf.definition({ on_list = on_list })
+      end, { desc = "Go to Definition" })
+      -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to References" })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+    end,
+  },
 }
