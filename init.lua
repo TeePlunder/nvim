88231b10be95d4a5585e23fb6ffc00f002e00e27
Leon Bergmann <l.bergmann@sweetconnect.io>
2025-12-01 16:48:11 +0100

    fix: use dark mode

--- init.lua
+++ init.lua
@@ -3,5 +3,5 @@ require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 
-vim.o.background = "light" -- or "light" for light mode
+vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
